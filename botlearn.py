b8d5d019da5487acef3594898978b7ca40ac5a6f
Pavan Shankar Koli <pskoli93@gmail.com>
2014-06-01 13:18:29 +0530

    Changed the input to the brain

--- botlearn.py
+++ botlearn.py
@@ -2,9 +2,8 @@ import aiml
 import os
 k=aiml.Kernel()
 k.verbose(True)
-k.learn("YourFilename.aiml") # The file to be learned by the bot. Note all the previous knowledge will get replaced, if that is not what you want than remove this line and uncomment the following two lines.
 '''
 k.loadBrain("Omegle.brn") # For just adding extra knowledge to the already created brain file.
-k.learn("Yourfilename.aiml")
 '''
+k.learn("YourFilename.aiml") # The file to be learned by the bot. Note all the previous knowledge will get replaced, if that is not what you want than remove this line and uncomment the above line.
 k.saveBrain("Omegle.brn")
