cdcd5e919cd6b4c0bbf69459641fb6679c0fc821
Pavan Shankar Koli <pskoli93@gmail.com>
2014-07-27 20:56:56 +0530

    Update botlearn.py

--- botlearn.py
+++ botlearn.py
@@ -5,5 +5,5 @@ k.verbose(True)
 '''
 k.loadBrain("Omegle.brn") # For just adding extra knowledge to the already created brain file.
 '''
-k.learn("YourFilename.aiml") # The file to be learned by the bot. Note all the previous knowledge will get replaced, if that is not what you want than remove this line and uncomment the above line.
+k.learn("YourFilename.aiml") # The file to be learned by the bot. Note all the previous knowledge will get replaced, if that is not what you intend than uncomment the above code.
 k.saveBrain("Omegle.brn")
